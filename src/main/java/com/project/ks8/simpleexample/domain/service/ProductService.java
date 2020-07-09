8236f7841cbf734334325adf6121c2c000ac28ba
fabricio <fabricio.jacob@outlook.com>
2020-07-09 16:38:16 -0300

    add

--- src/main/java/com/project/ks8/simpleexample/domain/service/ProductService.java
+++ src/main/java/com/project/ks8/simpleexample/domain/service/ProductService.java
@@ -4,11 +4,13 @@ import com.project.ks8.simpleexample.domain.exception.ProductNotFound;
 import com.project.ks8.simpleexample.domain.model.Product;
 import com.project.ks8.simpleexample.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -16,14 +18,17 @@ public class ProductService {
     private final ProductRepository repository;
 
     public List<Product> findAll() {
+        log.info("Realizando pesquisa de todos os produtos.");
         return repository.findAll();
     }
 
     public Product findlById(final Long id) {
+        log.info("Realizando pesquisa do produto: " + id);
         return repository.findById(id).orElseThrow(() -> new ProductNotFound("Product not found."));
     }
 
     public Product update(final Long id, final Product product) {
+        log.info("Atualizando o produto: " + id + ", informações: " + product.toString());
         return repository.findById(id)
                 .map(entity -> {
                     BeanUtils.copyProperties(product, entity, "id");

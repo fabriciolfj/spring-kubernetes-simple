c5b225a2a01ab8c7ae164d1166dfca312da01f2c
fabricio <fabricio.jacob@outlook.com>
2021-03-31 08:59:19 -0300

    add

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,5 +1,7 @@
+CREATE SEQUENCE product_id;
+
 create table product(
-id bigint not null auto_increment,
+id bigint not null DEFAULT nextval('product_id'),
 name varchar(100) not null,
 primary key (id)
-)engine=InnoDB default charset=utf8;
+);
